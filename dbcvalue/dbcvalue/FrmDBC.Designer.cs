56b1e88846d0a0f3f12a900544f9fc32c55637cd
zhyongquan <zhyongquan@gmail.com>
2018-04-26 16:30:21 +0800

    first release
    first release

--- dbcvalue/dbcvalue/FrmDBC.Designer.cs
+++ dbcvalue/dbcvalue/FrmDBC.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.grpMessage = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lblData = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numByte0 = new System.Windows.Forms.NumericUpDown();
-            this.cmbByteOrder = new System.Windows.Forms.ComboBox();
             this.dgvLayout = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numStartBit = new System.Windows.Forms.NumericUpDown();
-            this.numLength = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
+            this.numByte0 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.txtValue = new System.Windows.Forms.TextBox();
+            this.cmbByteOrder = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numLength = new System.Windows.Forms.NumericUpDown();
+            this.numStartBit = new System.Windows.Forms.NumericUpDown();
             this.grpMessage.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numByte0)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLayout)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numStartBit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numByte0)).BeginInit();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStartBit)).BeginInit();
             this.SuspendLayout();
             // 
             // grpMessage
@@ -56,65 +56,34 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.grpMessage.Controls.Add(this.dgvLayout);
-            this.grpMessage.Controls.Add(this.cmbByteOrder);
             this.grpMessage.Controls.Add(this.numByte0);
             this.grpMessage.Controls.Add(this.label4);
-            this.grpMessage.Controls.Add(this.label3);
             this.grpMessage.Controls.Add(this.lblData);
             this.grpMessage.Location = new System.Drawing.Point(12, 12);
             this.grpMessage.Name = "grpMessage";
-            this.grpMessage.Size = new System.Drawing.Size(667, 292);
+            this.grpMessage.Size = new System.Drawing.Size(667, 268);
             this.grpMessage.TabIndex = 0;
             this.grpMessage.TabStop = false;
             this.grpMessage.Text = "Message";
             // 
-            // groupBox2
+            // dgvLayout
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.dgvLayout.AllowUserToAddRows = false;
+            this.dgvLayout.AllowUserToDeleteRows = false;
+            this.dgvLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.txtValue);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.numLength);
-            this.groupBox2.Controls.Add(this.numStartBit);
-            this.groupBox2.Location = new System.Drawing.Point(12, 310);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(667, 138);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Signal";
-            // 
-            // lblData
-            // 
-            this.lblData.AutoSize = true;
-            this.lblData.Location = new System.Drawing.Point(16, 60);
-            this.lblData.Name = "lblData";
-            this.lblData.Size = new System.Drawing.Size(35, 12);
-            this.lblData.TabIndex = 0;
-            this.lblData.Text = "Data:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 29);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "ByteOrder:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 88);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 12);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Layout:";
+            this.dgvLayout.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLayout.Location = new System.Drawing.Point(81, 47);
+            this.dgvLayout.Name = "dgvLayout";
+            this.dgvLayout.ReadOnly = true;
+            this.dgvLayout.RowTemplate.Height = 23;
+            this.dgvLayout.Size = new System.Drawing.Size(574, 215);
+            this.dgvLayout.TabIndex = 6;
             // 
             // numByte0
             // 
-            this.numByte0.Location = new System.Drawing.Point(87, 58);
+            this.numByte0.Location = new System.Drawing.Point(81, 17);
             this.numByte0.Maximum = new decimal(new int[] {
             255,
             0,
@@ -125,6 +94,53 @@
             this.numByte0.TabIndex = 4;
             this.numByte0.ValueChanged += new System.EventHandler(this.numByte0_ValueChanged);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Layout:";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(10, 19);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(35, 12);
+            this.lblData.TabIndex = 0;
+            this.lblData.Text = "Data:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtValue);
+            this.groupBox2.Controls.Add(this.cmbByteOrder);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.numLength);
+            this.groupBox2.Controls.Add(this.numStartBit);
+            this.groupBox2.Location = new System.Drawing.Point(12, 286);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(667, 152);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Signal";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValue.Location = new System.Drawing.Point(81, 121);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.ReadOnly = true;
+            this.txtValue.Size = new System.Drawing.Size(574, 21);
+            this.txtValue.TabIndex = 6;
+            // 
             // cmbByteOrder
             // 
             this.cmbByteOrder.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
@@ -132,61 +148,51 @@
             this.cmbByteOrder.Items.AddRange(new object[] {
             "MSB",
             "LSB"});
-            this.cmbByteOrder.Location = new System.Drawing.Point(87, 26);
+            this.cmbByteOrder.Location = new System.Drawing.Point(81, 26);
             this.cmbByteOrder.Name = "cmbByteOrder";
             this.cmbByteOrder.Size = new System.Drawing.Size(53, 20);
             this.cmbByteOrder.TabIndex = 5;
             this.cmbByteOrder.SelectedIndexChanged += new System.EventHandler(this.cmbByteOrder_SelectedIndexChanged);
             // 
-            // dgvLayout
+            // label5
             // 
-            this.dgvLayout.AllowUserToAddRows = false;
-            this.dgvLayout.AllowUserToDeleteRows = false;
-            this.dgvLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvLayout.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLayout.Location = new System.Drawing.Point(87, 88);
-            this.dgvLayout.Name = "dgvLayout";
-            this.dgvLayout.ReadOnly = true;
-            this.dgvLayout.RowTemplate.Height = 23;
-            this.dgvLayout.Size = new System.Drawing.Size(574, 198);
-            this.dgvLayout.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "StartBit:";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Value:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Location = new System.Drawing.Point(10, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "Length:";
             // 
-            // numStartBit
+            // label1
             // 
-            this.numStartBit.Location = new System.Drawing.Point(81, 29);
-            this.numStartBit.Maximum = new decimal(new int[] {
-            16,
-            0,
-            0,
-            0});
-            this.numStartBit.Name = "numStartBit";
-            this.numStartBit.Size = new System.Drawing.Size(53, 21);
-            this.numStartBit.TabIndex = 4;
-            this.numStartBit.ValueChanged += new System.EventHandler(this.numStartBit_ValueChanged);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "StartBit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ByteOrder:";
             // 
             // numLength
             // 
-            this.numLength.Location = new System.Drawing.Point(81, 61);
+            this.numLength.Location = new System.Drawing.Point(81, 90);
             this.numLength.Maximum = new decimal(new int[] {
             16,
             0,
@@ -207,28 +213,29 @@
             0});
             this.numLength.ValueChanged += new System.EventHandler(this.numStartBit_ValueChanged);
             // 
-            // label5
+            // numStartBit
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 95);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 12);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Value:";
-            // 
-            // txtValue
-            // 
-            this.txtValue.Location = new System.Drawing.Point(81, 92);
-            this.txtValue.Name = "txtValue";
-            this.txtValue.ReadOnly = true;
-            this.txtValue.Size = new System.Drawing.Size(100, 21);
-            this.txtValue.TabIndex = 6;
+            this.numStartBit.Location = new System.Drawing.Point(81, 58);
+            this.numStartBit.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numStartBit.Name = "numStartBit";
+            this.numStartBit.Size = new System.Drawing.Size(53, 21);
+            this.numStartBit.TabIndex = 4;
+            this.numStartBit.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numStartBit.ValueChanged += new System.EventHandler(this.numStartBit_ValueChanged);
             // 
             // FrmDBC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 460);
+            this.ClientSize = new System.Drawing.Size(691, 450);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.grpMessage);
             this.Name = "FrmDBC";
@@ -236,12 +243,12 @@
             this.Load += new System.EventHandler(this.FrmDBC_Load);
             this.grpMessage.ResumeLayout(false);
             this.grpMessage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLayout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numByte0)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numByte0)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvLayout)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numStartBit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStartBit)).EndInit();
             this.ResumeLayout(false);
 
         }
--- dbcvalue/dbcvalue/FrmDBC.cs
+++ dbcvalue/dbcvalue/FrmDBC.cs
@@ -13,6 +13,7 @@ namespace dbcvalue
     {
         private List<NumericUpDown> listNumByte = new List<NumericUpDown>();
         private byte[] Data = new byte[8];
+        private string bin = "";
         public FrmDBC()
         {
             InitializeComponent();
@@ -20,49 +21,122 @@ namespace dbcvalue
 
         private void FrmDBC_Load(object sender, EventArgs e)
         {
-            cmbByteOrder.SelectedIndex = 0;
+            //create control array
             listNumByte.Clear();
             listNumByte.Add(numByte0);
+            Random rd = new Random();
             for (int i = 1; i < 8; i++)
             {
                 NumericUpDown ctrl = new NumericUpDown();
                 ctrl.Name = "numByte" + i.ToString();
                 ctrl.Maximum = byte.MaxValue;
+                ctrl.Value = rd.Next(byte.MinValue, byte.MaxValue);
                 ctrl.Location = new Point(numByte0.Location.X + i * 60, numByte0.Location.Y);
                 ctrl.Size = numByte0.Size;
                 ctrl.ValueChanged += numByte0_ValueChanged;
                 grpMessage.Controls.Add(ctrl);
+                listNumByte.Add(ctrl);
             }
+            //add columns
             for (int i = 0; i < 8; i++)
             {
-                DataGridViewColumn col = new DataGridViewColumn();
+                DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn();//DataGridViewTextBoxColumn, not DataGridViewColumn
                 col.Name = i.ToString();
                 col.HeaderText = (7 - i).ToString();
                 col.Width = 30;
                 dgvLayout.Columns.Add(col);
             }
-            dgvLayout.Rows.Add();
-        }
-        private void numByte0_ValueChanged(object sender, EventArgs e)
-        {
+            //add rows
+            for (int i = 0; i < 8; i++)
+            {
+                int index = dgvLayout.Rows.Add();
+                dgvLayout.Rows[i].HeaderCell.Value = i.ToString();
+            }
             //get data
+            MessageDataChanged();
+            cmbByteOrder.SelectedIndex = 0;
+        }
+        private void MessageDataChanged()
+        {
+            bin = "";
             for (int i = 0; i < 8; i++)
             {
                 Data[i] = (byte)listNumByte[i].Value;
+                string str = Convert.ToString(Data[i], 2).PadLeft(8, '0');
+                bin += str;
+                //draw layout
+                for (int j = 0; j < 8; j++)
+                {
+                    dgvLayout.Rows[i].Cells[7 - j].Value = str[j];
+                }
             }
-            //draw layout
-
+        }
+        private void numByte0_ValueChanged(object sender, EventArgs e)
+        {
+            MessageDataChanged();
+            SignalDataChange();
             //MessageBox.Show("new value:" + (sender as NumericUpDown).Value.ToString());
         }
+        private void SignalDataChange()
+        {
+            int startbit = (int)(numStartBit.Value);
+            int length = (int)(numLength.Value);
+            int index = 0; int count = 0; bool flag = false;
+            string value = "";
+            if (cmbByteOrder.Text == "MSB")
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        index = i * 8 + 7 - j;
+                        if (index == startbit)
+                        {
+                            flag = true;
+                        }
+                        if (flag && count < length)
+                        {
+                            dgvLayout.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                            count++;
+                            value += bin[index];
+                        }
+                        else
+                        {
+                            dgvLayout.Rows[i].Cells[j].Style.BackColor = Color.White;
+                        }
+                    }
+                }
 
+            }
+            else
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 7; j >= 0; j--)
+                    {
+                        index = i * 8 + 7 - j;
+                        if (index >= startbit && index <= startbit + length)
+                        {
+                            dgvLayout.Rows[i].Cells[j].Style.BackColor = Color.Yellow;
+                            value = bin[index] + value;
+                        }
+                        else
+                        {
+                            dgvLayout.Rows[i].Cells[j].Style.BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+            txtValue.Text = string.Format("bin={0},dec={1}", value, Convert.ToInt32(value, 2));
+        }
         private void cmbByteOrder_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            SignalDataChange();
         }
 
         private void numStartBit_ValueChanged(object sender, EventArgs e)
         {
-
+            SignalDataChange();
         }
     }
 }
